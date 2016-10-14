d27193e10c34f090b9293556a84fd5cf13366564
RamonSchmitt <ramon.schmitt@yahoo.com>
2016-10-14 10:39:16 +0200

    comments

--- src/routes.jsx
+++ src/routes.jsx
@@ -3,6 +3,7 @@ var ReactRouter = require('react-router');
 var Router = ReactRouter.Router;
 var Route = ReactRouter.Route;
 var History = ReactRouter.hashHistory;
+var ImageDetail = require('./components/image-detail');
 
 var Main = require('./components/main');
 var Topic = require('./components/topic');
@@ -11,6 +12,7 @@ module.exports = (
   <Router history={History}>
     <Route path="/" component={Main}>
       <Route path="topics/:id" component={Topic} />
+      <Route path="images/:id" component={ImageDetail} />
     </Route>
   </Router>
 );
